abb5025f18a62151e0cc00a5d4a0f64a009a05fd
Farlon <f.souto@sportradar.com>
2022-04-06 01:50:56 +0200

    Will add the calls to the GUI sample

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from Customer import Customer
+from Customer import *
+from UserInterface import *
 
 customer = Customer("Farlon", "12345678", "my.email@provider.com", "Ola Nordman Gt 123")
 customer.toString()
+
+guiRoot = tk.Tk()
+gui = UserInterface(guiRoot)
+guiRoot.title("Streaming App")
+guiRoot.geometry("640x480")
+guiRoot.mainloop()
+
